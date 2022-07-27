a18b473843be3e473bcebb905316b9078a7ceeb4
CamilaMelgar <98069661+CamilaMelgar@users.noreply.github.com>
2022-07-26 22:08:37 -0300

    Se agrega logica para almacenar en el local storage los items del carrito y reestablecerlos cada vez que el usuario ingresa a la página.

--- js/script.js
+++ js/script.js
@@ -18,6 +18,8 @@ if (document.readyState == 'loading'){
 }
 
 function ready(){
+    // localStorage.clear()
+
     let removeCartButtons = document.getElementsByClassName('cart-remove');
     for (let i = 0; i < removeCartButtons.length; i++){
         let button = removeCartButtons[i];
@@ -37,6 +39,15 @@ function ready(){
     }
 
     document.getElementsByClassName("btn-buy")[0].addEventListener("click", buyButtonClicked)
+
+    let products = JSON.parse(localStorage.getItem("products"))
+
+    if (products != null){
+         for (let i = 0; i < products.length; i ++){
+            addProductToCart(products[i].name, products[i].price, products[i].image, products[i].quantity)
+        }
+        updateTotal()
+    }
 }
 
 function buyButtonClicked(event){
@@ -46,6 +57,7 @@ function buyButtonClicked(event){
     while(cartContent.hasChildNodes()){
         cartContent.removeChild(cartContent.firstChild)
     }
+    localStorage.clear()
     updateTotal()
 }
 
@@ -69,11 +81,12 @@ function addCartClicked(event){
     let title = shopProducts.getElementsByClassName("product-title")[0].innerText;
     let price = shopProducts.getElementsByClassName("price")[0].innerText;
     let productImg = shopProducts.getElementsByClassName("product-img")[0].src;
-    addProductToCart(title, price, productImg)
+    addProductToCart(title, price, productImg, null)
+
     updateTotal()
 }
 
-function addProductToCart(title, price, productImg){
+function addProductToCart(title, price, productImg, quantity){
     let cartShopBox = document.createElement("div");
     cartShopBox.classList.add("cart-box");
     let cartItems = document.getElementsByClassName("cart-content")[0];
@@ -85,34 +98,56 @@ function addProductToCart(title, price, productImg){
         }
     }
 
+    let qty = 1
+    if (quantity != null){
+        qty = quantity
+    }
+
     let cartBoxContent = `
                             <img src="${productImg}" alt="" class="cart-img">
                             <div class="detail-box">
                                 <div class="cart-product-title">${title}</div>
                                 <div class="cart-price">${price }</div>
-                                <input type="number" value="1" class="cart-quantity">
+                                <input type="number" value="${qty}" class="cart-quantity">
                             </div>
                             <i class="bx bxs-trash-alt cart-remove"></i>`;
     cartShopBox.innerHTML = cartBoxContent;
     cartItems.append(cartShopBox);
     cartShopBox.getElementsByClassName("cart-remove")[0].addEventListener('click', removeCartItem)
     cartShopBox.getElementsByClassName("cart-quantity")[0].addEventListener('click', quantityChanged)
-                            
-    
 }
 
 function updateTotal(){
     let cartContent = document.getElementsByClassName("cart-content")[0];
     let cartBoxes = document.getElementsByClassName("cart-box");
+    let badge = document.getElementsByClassName("icon-badge")[0];
     let total = 0;
+
+    let products = [];
+    let totalQuantity = 0;
+
     for (let i = 0; i < cartBoxes.length; i++){
         let cartBox = cartBoxes[i];
+        let img = cartBox.getElementsByClassName("cart-img")[0].src
+        let name = cartBox.getElementsByClassName("cart-product-title")[0].innerHTML
         let priceElement = cartBox.getElementsByClassName("cart-price")[0];
         let quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
         let quantity = quantityElement.value;
         let price = parseFloat(priceElement.innerText.replace("$", ""));
+
+        totalQuantity = totalQuantity + parseInt(quantity)
+
+        let product = {name : name, image : img, price : price, quantity : quantity}
+        products.push(product)
+
         total = total + price * quantity;
         total = Math.round(total * 100) / 100
     }
+
+    let jsonProducts = JSON.stringify(products)
+    localStorage.setItem("products", jsonProducts)
+    localStorage.setItem("total", total)
+
+    badge.innerHTML = totalQuantity.toString()
     document.getElementsByClassName("total-price")[0].innerText = "$" + total; 
 }
